23015c230489421b7ccdbac8b7350589a944232b
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-10-26 16:37:52 -0300

    code refactoring

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/src/Helpers/functions.php';
 require_once __DIR__ . '/config/app.php';
-require_once __DIR__ . '/autoload.php';
+require_once __DIR__ . '/src/lib/autoload.php';
 
 use lib\Router;
 
--- src/lib/autoload.php
+++ src/lib/autoload.php
@@ -2,7 +2,7 @@
 
 function autoload($class)
 {
-    $dirBase = __DIR__ . DS;
+    $dirBase = DIR_APP;
     $class = $dirBase . 'src' . DS . str_replace('\\', DS, $class) . '.php';
 
     if (file_exists($class) && !is_dir($class)) include $class;
