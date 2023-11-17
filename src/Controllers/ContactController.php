34f2b4b27f2cc9e9889cc4d196772ef02e3c2715
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-11-17 13:32:03 -0300

    refactoring code

--- src/Controllers/ContactController.php
+++ src/Controllers/ContactController.php
@@ -2,6 +2,10 @@
 
 namespace Controllers;
 
+use Exceptions\CustomException;
+use Handlers\Contact\StoreHandler;
+use Handlers\Contact\UpdateHandler;
+use Handlers\ValidationHandler;
 use lib\ConnectionFactory;
 use Models\DAO\ContactDAO;
 use Models\Contact;
@@ -11,17 +15,19 @@ use Sessions\Session;
 
 class ContactController
 {
-    private $jsonResource;
-    private $jsonRequestService;
-    private $validate;
-    private $connection;
-    private $user;
+    public $jsonResource;
+    public $jsonRequestService;
+    public $validate;
+    public $connection;
+    public $user;
+    public $validationHandler;
 
     public function __construct(array $dependency)
     {
         $this->jsonResource = $dependency['Resources\JsonResource'];
         $this->jsonRequestService = $dependency['Services\JsonRequestService'];
         $this->validate = $dependency['Validate\Validate'];
+        $this->validationHandler = $dependency['Handlers\ValidationHandler'];
         $this->connection = ConnectionFactory::getConnection();
         $this->user = Session::get('user');
     }
@@ -31,19 +37,15 @@ class ContactController
         echo 'Contacts';
     }
 
-    public function store(): JsonResource
+    public function store()
     {
-        $ContactDAO = new ContactDAO($this->connection);
-        $Contact = new Contact();
-
-        $data = $this->jsonRequestService->getData();
-
-        $rules = [
+        $data['rules'] = [
             'name' => [
                 'required' => true,
                 'min' => 3,
                 'max' => 255,
-                'regex' => '/^[a-zA-ZÀ-ÿ\s]+$/u'
+                'regex' => '/^[a-zA-ZÀ-ÿ\s]+$/u',
+                'contact-unique' => 'name|contacts|user_id|' . $this->user['id']
             ],
             'email' => [
                 'required' => false,
@@ -53,90 +55,110 @@ class ContactController
             ]
         ];
 
-        $this->validate->validate($data, $rules);
-        $_errors = $this->validate->getErrors() ?? [];
+        $validationHandler = new ValidationHandler();
+        $storeHandler = new StoreHandler();
 
-        foreach ($_errors as $key => $value) $errors[] = $value;
-
-        if ($this->validate->passed()) {
-            $data['user_id'] = $this->user['id'];
-            $data['email'] = $data['email'] ?? null;
-
-            $Contact->setUserId($data['user_id']);
-            $Contact->setName($data['name']);
-            $Contact->setEmail($data['email']);
-            $resultId = $ContactDAO->register($Contact);
-
-            if (!!$resultId) {
-                unset($data);
-
-                $Contact->setId($resultId);
-                $data = $ContactDAO->getContactById($Contact);
-                $data['email'] = (!is_null($data['email'])) ? $data['email'] : 'Não informado';
-
-                return $this->jsonResource->toJson(201, 'Contato cadastrado com sucesso!', ['data' => $data]);
-            } else
-                return $this->jsonResource->toJson(500, 'Houve um erro interno.');
-        } else
-            return $this->jsonResource->toJson(422, 'Erro ao tentar cadastrar o contato.', ['errors' => $errors]);
+        $validationHandler->setSuccessor($storeHandler);
+        $validationHandler->handle($data, $this);
     }
 
     public function update($params)
     {
         $contactId = (!filter_var($params['id'], FILTER_VALIDATE_INT) === false) ? $params['id'] : false;
-        if (!$contactId) return $this->jsonResource->toJson(404, 'Página não encontrada.');
+
+        if (!$contactId) throw new CustomException('Contato inexistente.', 404);
 
         $ContactDAO = new ContactDAO($this->connection);
+
         $Contact = new Contact();
-
         $Contact->setId($contactId);
-        $result = $ContactDAO->getContactById($Contact);
 
-        if (!!$result) {
-            $userId = $this->user['id'];
-            $ownerId = $result['user_id'];
+        if (!$contact = $ContactDAO->getContactById($Contact)) throw new CustomException('Contato inexistente.', 404);
 
-            if (AuthorizationService::checkOwner($userId, $ownerId)) {
-                $data = $this->jsonRequestService->getData();
+        $data['rules'] = [
+            'name' => [
+                'required' => true,
+                'min' => 3,
+                'max' => 255,
+                'regex' => '/^[a-zA-ZÀ-ÿ\s]+$/u',
+                'contact-unique-for-update' => 'name|contacts|user_id|' . $this->user['id'] . '|id|' . $contact['id']
+            ],
+            'email' => [
+                'required' => false,
+                'email' => true,
+                'max' => 128,
+                'contact-unique-for-update' => 'email|contacts|user_id|' . $this->user['id'] . '|id|' . $contact['id']
+            ]
+        ];
+        
+        $data['contact_id'] = $contact['id'];
 
-                $rules = [
-                    'name' => [
-                        'required' => true,
-                        'min' => 3,
-                        'max' => 255,
-                        'regex' => '/^[a-zA-ZÀ-ÿ\s]+$/u',
-                    ],
-                    'email' => [
-                        'required' => false,
-                        'email' => true,
-                        'max' => 128,
-                        'contact-unique-for-update' => 'email|contacts|user_id|' . $userId . '|id|' . $result['id']
-                    ]
-                ];
+        $validationHandler = new ValidationHandler();
+        $updateHandler = new UpdateHandler();
 
-                $this->validate->validate($data, $rules);
-                $_errors = $this->validate->getErrors() ?? [];
-
-                foreach ($_errors as $key => $value) $errors[] = $value;
-                
-                if ($this->validate->passed()) {
-                    $data['email'] = $data['email'] ?? null;
-
-                    $Contact->setId($result['id']);
-                    $Contact->setName($data['name']);
-                    $Contact->setEmail($data['email']);
-
-                    $result = $ContactDAO->update($Contact);
-
-                    if ($result) {
-                        return $this->jsonResource->toJson(204); // 204 (_NO_CONTENT?)
-                    } else
-                        return $this->jsonResource->toJson(500, 'Houve um erro interno.');
-                } else
-                    return $this->jsonResource->toJson(422, 'Error ao tentar atualizar o cadastro.', ['errors' => $errors]);
-            } else
-                return $this->jsonResource->toJson(401, 'Você não tem permissão para executar esta ação.');
-        } else
-            return $this->jsonResource->toJson(404, 'Contato inexistente.');
+        $validationHandler->setSuccessor($updateHandler);
+        $validationHandler->handle($data, $this);
+        // ...
     }
+
+    // public function update($params)
+    // {
+    //     $contactId = (!filter_var($params['id'], FILTER_VALIDATE_INT) === false) ? $params['id'] : false;
+    //     if (!$contactId) return $this->jsonResource->toJson(404, 'Página não encontrada.');
+
+    //     $ContactDAO = new ContactDAO($this->connection);
+    //     $Contact = new Contact();
+
+    //     $Contact->setId($contactId);
+    //     $result = $ContactDAO->getContactById($Contact);
+
+    //     if (!!$result) {
+    //         $userId = $this->user['id'];
+    //         $ownerId = $result['user_id'];
+
+    //         if (AuthorizationService::checkOwner($userId, $ownerId)) {
+    //             $data = $this->jsonRequestService->getData();
+
+    //             $rules = [
+    //                 'name' => [
+    //                     'required' => true,
+    //                     'min' => 3,
+    //                     'max' => 255,
+    //                     'regex' => '/^[a-zA-ZÀ-ÿ\s]+$/u',
+    //                 ],
+    //                 'email' => [
+    //                     'required' => false,
+    //                     'email' => true,
+    //                     'max' => 128,
+    //                     'contact-unique-for-update' => 'email|contacts|user_id|' . $userId . '|id|' . $result['id']
+    //                 ]
+    //             ];
+
+    //             $this->validate->validate($data, $rules);
+    //             $_errors = $this->validate->getErrors() ?? [];
+
+    //             foreach ($_errors as $key => $value) $errors[] = $value;
+
+    //             if ($this->validate->passed()) {
+    //                 $data['email'] = $data['email'] ?? null;
+
+    //                 $Contact->setId($result['id']);
+    //                 $Contact->setName($data['name']);
+    //                 $Contact->setEmail($data['email']);
+
+    //                 $result = $ContactDAO->update($Contact);
+    //                 unset($data);
+    //                 if (!!$data = $ContactDAO->getContactById($Contact)) {
+    //                     $data['email'] = (empty($data['email']) || is_null($data['email'])) ? 'Não informado' : $data['email'];
+
+    //                     return $this->jsonResource->toJson(200, 'Contato atualizado com sucesso!', ['data' => $data]);
+    //                 } else
+    //                     return $this->jsonResource->toJson(500, 'Houve um erro interno.');
+    //             } else
+    //                 return $this->jsonResource->toJson(422, 'Error ao tentar atualizar o cadastro.', ['errors' => $errors]);
+    //         } else
+    //             return $this->jsonResource->toJson(401, 'Você não tem permissão para executar esta ação.');
+    //     } else
+    //         return $this->jsonResource->toJson(404, 'Contato inexistente.');
+    // }
 }
