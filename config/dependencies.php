7c9cd3ff4e0a4ae1b6ea63be09a8f2295b986b83
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-11-17 15:21:51 -0300

    change in dependencies

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,11 +1,13 @@
 <?php
 
+use lib\ConnectionFactory;
 use Resources\JsonResource;
 use Services\JsonRequestService;
 use Validate\Validate;
 
 return [
-    JsonResource::class => new JsonResource(),
+    ConnectionFactory::class => ConnectionFactory::getConnection(),
     JsonRequestService::class => new JsonRequestService(),
+    JsonResource::class => new JsonResource(),
     Validate::class => new Validate()
 ];
