973711fb8ec4da99b6a469a302061fcb3bea530d
Bruno Coelho <eu.brunocoelho94@gmail.com>
2023-11-23 14:01:07 -0300

    small modifications

--- src/Middlewares/Cors.php
+++ src/Middlewares/Cors.php
@@ -9,7 +9,7 @@ class Cors
         header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Headers: access');
         header('Access-Control-Allow-Methods: ' . $method);
-        // header('Content-Type: application/json; charset=UTF-8');
+        header('Content-Type: application/json; charset=UTF-8');
         header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
     }
 }
